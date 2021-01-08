0d7afda32a82606a68f3f5a1550197bea98ea287
Tom Valk <tomvalk@gmail.com>
2021-01-08 18:20:28 +0200

    Implement informative pages and new design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,13 @@
-import {Component, Injectable, OnInit} from '@angular/core';
-import { loadData } from './app.data';
-import { HttpClient } from '@angular/common/http';
-import { PeriodicTable } from './app.model';
+import { Component, Injectable } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.scss' ]
 })
 @Injectable()
-export class AppComponent implements OnInit {
-  title = 'past';
-  table: PeriodicTable = null;
+export class AppComponent {
+	title = 'PAST Framework';
 
-  weight1: any = 0.5;
-  weight2: any = 0.5;
-
-  constructor(private http: HttpClient) {}
-
-  async ngOnInit(): Promise<any> {
-    this.table = await loadData(this.http, this.weight1);
-  }
-
-  async changeWeights(newValue): Promise<void> {
-    this.weight1 = newValue;
-    this.weight2 = Math.round((1 - newValue) * 100) / 100;
-
-    this.table = await loadData(this.http, this.weight1);
-  }
+	constructor() {}
 }
