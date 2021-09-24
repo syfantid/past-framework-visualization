02949f3f64d1e98768fd72df40606ea08600fd53
Tom Valk <tomvalk@lt-box.info>
2021-09-24 17:35:50 +0300

    [TASK] Add new features to the project: filters

--- src/app/app.data.ts
+++ src/app/app.data.ts
@@ -2,6 +2,159 @@ import { PeriodicTable } from './app.model';
 import { HttpClient } from '@angular/common/http';
 import * as Papa from 'papaparse';
 
+
+export function combineSubset(template, freqs, efcs, userWeight) {
+  for (const idx in template) {
+    if (! template.hasOwnProperty(idx)) {
+      continue;
+    }
+    template[idx].efficacy = efcs[template[idx].technique];
+    template[idx].frequency = freqs[template[idx].technique];
+  }
+
+  let data = [];
+
+  // Calculate weighted scores.
+  for (const idx in template) {
+    if (! template.hasOwnProperty(idx)) {
+      continue;
+    }
+    const row = template[idx];
+    data[idx] = template[idx];
+    data[idx].weightedScore = calculateWeightedSum(
+      row.efficacy, row.frequency, userWeight
+    );
+  }
+
+  // Normalize weighted scores to determinate the score.
+  data = normalizeScores(data);
+
+  // Group by category.
+  const categoryData: any = {'Primary Task Support': [], Dialogue: [], 'Social Support': [], 'System Credibility': [], Other: []};
+  for (const row of data) {
+    if (! categoryData.hasOwnProperty(row.category)) {
+      categoryData[row.category] = [];
+    }
+    categoryData[row.category].push(row);
+  }
+
+  // Sort categories.
+  for (const cat in categoryData) {
+    categoryData[cat] = categoryData[cat].sort((a, b) => {return a.finalScore > b.finalScore ? -1 : a.finalScore < b.finalScore ? 1 : 0});
+  }
+
+
+
+  // Create table.
+  const table = new PeriodicTable();
+  for (const cat in categoryData) {
+    const categoryColumn = table.addColumn(cat);
+    for (const row of categoryData[cat]) {
+      categoryColumn.add(row.slug, row.name, row.finalScore);
+    }
+  }
+
+  return table;
+}
+
+
+/**
+ * Filter subset
+ * @param input
+ * @param cohort
+ * @param minSample
+ * @param minDuration
+ */
+export function filterSubset(input: any, cohort: string[], minSample: number, minDuration: number): any {
+  const output = [];
+  for (const idx in input) {
+    if (! input.hasOwnProperty(idx)) {
+      continue;
+    }
+
+    if (minSample !== null && minSample > 0) {
+      if (input[idx].sample_size < minSample) {
+        continue;
+      }
+    }
+
+    if (minDuration !== null && minDuration > 0) {
+      if (input[idx].duration < minDuration) {
+        continue;
+      }
+    }
+
+    if (cohort.length > 0) {
+      const intersection = cohort.filter(value => input[idx].cohort.includes(value));
+      if (intersection.length === 0) {
+        continue;
+      }
+    }
+
+    output.push(input[idx]);
+  }
+
+  return output;
+}
+
+
+export function calculateFrequency(input: any): any {
+  const techCounts: {} = {};
+
+  for (const idx in input) {
+    if (! input.hasOwnProperty(idx)) {
+      continue;
+    }
+    for (const catNr of input[idx].techniques) {
+      if (! techCounts.hasOwnProperty(catNr)) {
+        techCounts[catNr] = 0;
+      }
+      techCounts[catNr]++;
+    }
+  }
+
+  for (const idx in techCounts) {
+    if (! techCounts.hasOwnProperty(idx)) {
+      continue;
+    }
+    techCounts[idx] = techCounts[idx] / input.length;
+  }
+
+  return techCounts;
+}
+
+
+export function calculateEfficacy(input: any): any {
+  const techCounts: {} = {};
+  const techSum: {} = {};
+
+  for (const idx in input) {
+    if (! input.hasOwnProperty(idx)) {
+      continue;
+    }
+    for (const catNr of input[idx].techniques) {
+      if (! techCounts.hasOwnProperty(catNr)) {
+        techCounts[catNr] = 0;
+      }
+      if (! techSum.hasOwnProperty(catNr)) {
+        techSum[catNr] = 0;
+      }
+      techSum[catNr] += parseFloat(input[idx].techniques_weighted_result);
+      techCounts[catNr]++;
+    }
+  }
+
+  for (const idx in techCounts) {
+    if (! techCounts.hasOwnProperty(idx)) {
+      continue;
+    }
+    techSum[idx] = techSum[idx] / techCounts[idx];
+  }
+
+  return techSum;
+}
+
+
 /**
  * Calculate weighted sum.
  *
@@ -123,13 +276,127 @@ function processCsv(rawData: string, userWeight: number): PeriodicTable {
   return table;
 }
 
-// Add data from CSV file.
-export async function loadData(http: HttpClient, userWeight: number = 0.5): Promise<PeriodicTable> {
+/**
+ * Processing CSV file.
+ *
+ * @param rawData
+ * @param userWeight
+ */
+function processInputCsv(rawData: string, userWeight: number): PeriodicTable {
+  const parsed = Papa.parse(rawData, {header: true});
+  if (!parsed.data) {
+    return null;
+  }
+
+  let data = [];
+
+  // Calculate weighted scores.
+  for (const idx in parsed.data) {
+    if (! parsed.data.hasOwnProperty(idx)) {
+      continue;
+    }
+    const row = parsed.data[idx];
+    if (! row.hasOwnProperty('efficacy')) {
+      continue;
+    }
+    data[idx] = parsed.data[idx];
+    data[idx].weightedScore = calculateWeightedSum(
+      Number.parseFloat(row.efficacy), Number.parseFloat(row.frequency), userWeight
+    );
+  }
+
+  // Normalize weighted scores to determinate the score.
+  data = normalizeScores(data);
+
+  // Group by category.
+  const categoryData: any = {'Primary Task Support': [], Dialogue: [], 'Social Support': [], 'System Credibility': [], Other: []};
+  for (const row of data) {
+    if (! categoryData.hasOwnProperty(row.category)) {
+      categoryData[row.category] = [];
+    }
+    categoryData[row.category].push(row);
+  }
+
+  // Sort categories.
+  for (const cat in categoryData) {
+    categoryData[cat] = categoryData[cat].sort((a, b) => {return a.finalScore > b.finalScore ? -1 : a.finalScore < b.finalScore ? 1 : 0});
+  }
+
+
+
+  // Create table.
+  const table = new PeriodicTable();
+  for (const cat in categoryData) {
+    const categoryColumn = table.addColumn(cat);
+    for (const row of categoryData[cat]) {
+      categoryColumn.add(row.slug, row.name, row.finalScore);
+    }
+  }
+
+  return table;
+}
+
+/**
+ * Processing Simple CSV file.
+ *
+ * @param rawData
+ */
+function processSimpleCsv(rawData: string): any {
+  const parsed = Papa.parse(rawData, {header: true});
+  if (!parsed.data) {
+    return null;
+  }
+  delete parsed.data[parsed.data.length - 1];
+  const data = parsed.data;
+
+  for (const idx in data) {
+    if (! data.hasOwnProperty(idx)) {
+      continue;
+    }
+    const row = data[idx];
+    if (row.hasOwnProperty('cohort')) {
+      if (row.cohort) {
+        row.cohort = row.cohort.split('|');
+      } else {
+        row.cohort = [];
+      }
+    }
+    if (row.hasOwnProperty('techniques')) {
+      if (row.techniques) {
+        row.techniques = row.techniques.split('|');
+      } else {
+        row.techniques = [];
+      }
+    }
+  }
+
+  return parsed.data;
+}
+
+
+// Load the calculations input file.
+export async function loadInputFile(http: HttpClient): Promise<any> {
   return new Promise((resolve, reject) => {
-    http.get('assets/past_output.csv', {responseType: 'text'})
+    http.get('assets/calculations_input.csv', {responseType: 'text'})
       .subscribe(
         data => {
-          return resolve(processCsv(data, userWeight));
+          return resolve(processSimpleCsv(data));
+        },
+        error => {
+          return resolve(null);
+        }
+      );
+  });
+}
+
+
+// Load the mapping input file.
+export async function loadMappingFile(http: HttpClient): Promise<any> {
+  return new Promise((resolve, reject) => {
+    http.get('assets/mappings_technique_names.csv', {responseType: 'text'})
+      .subscribe(
+        data => {
+          return resolve(processSimpleCsv(data));
         },
         error => {
           return resolve(null);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,91 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { loadData } from '../app.data';
-import { HttpClient } from '@angular/common/http';
-import { PeriodicTable } from '../app.model';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {loadInputFile, loadMappingFile, filterSubset, calculateFrequency, calculateEfficacy, combineSubset} from '../app.data';
+import {HttpClient} from '@angular/common/http';
+import {PeriodicTable} from '../app.model';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	styleUrls: [ './home.component.scss' ]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 @Injectable()
 export class HomeComponent implements OnInit {
-	title = 'past';
-	table: PeriodicTable = null;
+  title = 'past';
+  table: PeriodicTable = null;
+  template: any = null;
+  inputs: any = null;
 
-	weight1: any = 0.5;
-	weight2: any = 0.5;
+  weight1: any = 0.5;
+  weight2: any = 0.5;
 
-	constructor(private http: HttpClient) {}
+  cohort: string[] = [];
+  minDuration: number = null;
+  minSampleSize: number = null;
 
-	async ngOnInit(): Promise<any> {
-		this.table = await loadData(this.http, this.weight1);
-	}
+  cohortOptions = [
+    {name: 'General population', selected: false},
+    {name: 'Children', selected: false},
+    {name: 'Adolescents', selected: false},
+    {name: 'Elderly', selected: false},
+    {name: 'Families', selected: false},
+    {name: 'Racial minority', selected: false},
+    {name: 'Women only', selected: false},
+    {name: 'Men only', selected: false},
+    {name: 'Amateur athletes', selected: false},
+    {name: 'University students', selected: false},
+    {name: 'Office workers', selected: false},
+    {name: 'Inactive population', selected: false},
+    {name: 'Overweight population', selected: false},
+    {name: 'Obese population', selected: false},
+    {name: 'Patients with cancer', selected: false},
+    {name: 'Patients with diabetes', selected: false},
+    {name: 'Patients with heart disease', selected: false},
+    {name: 'Patients with serious mental illness', selected: false},
+    {name: 'Cancer survivors', selected: false},
+  ];
 
-	async changeWeights(newValue): Promise<void> {
-		this.weight1 = newValue;
-		this.weight2 = Math.round((1 - newValue) * 100) / 100;
+  constructor(private http: HttpClient) {
+  }
 
-		this.table = await loadData(this.http, this.weight1);
-	}
+  async ngOnInit(): Promise<any> {
+    this.template = await loadMappingFile(this.http);
+    this.inputs = await loadInputFile(this.http);
+    console.log(this.template);
+    console.log(this.inputs);
+
+    this.applyFilters();
+
+    // this.table = await loadData(this.http, this.weight1);
+  }
+
+  async applyFilters() {
+    const subset = filterSubset(this.inputs, this.cohort, this.minSampleSize, this.minDuration);
+    const frequencies = calculateFrequency(subset);
+    const efficacies = calculateEfficacy(subset);
+    this.table = combineSubset(this.template, frequencies, efficacies, this.weight1);
+  }
+
+  async changeWeights(newValue): Promise<void> {
+    this.weight1 = newValue;
+    this.weight2 = Math.round((1 - newValue) * 100) / 100;
+    // this.table = await loadData(this.http, this.weight1);
+    this.applyFilters();
+  }
+
+  async changeCohort(): Promise<void> {
+    this.cohort = [];
+    for (const cohort of this.cohortOptions) {
+      if (cohort.selected) {
+        this.cohort.push(cohort.name);
+      }
+    }
+    this.applyFilters();
+  }
+
+  async changeDuration(): Promise<void> {
+    this.applyFilters();
+  }
+  async changeSampleSize(): Promise<void> {
+    this.applyFilters();
+  }
 }
